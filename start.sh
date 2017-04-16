151c7f7c6ae18f132007e79fbf9fde809b4ad825
z3cr3t <estradadionegil@gmail.com>
2017-04-17 02:54:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
+### BEGIN INIT INFO
+# Provides:          mISTBOX
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+#BRIDGE START
 virsh net-define /etc/libvirt/qemu/networks/br1.xml &&
 virsh net-start br1 &&
 #port forwarding nat and rdp
